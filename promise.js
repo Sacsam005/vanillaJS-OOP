a2b372ad1dc4f892b653d02ca23e2cb562e5a307
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-12-13 14:01:16 -0600

    Create promise.js

---
+++ promise.js
@@ -0,0 +1,24 @@
+// promise - resolve, reject, pending
+
+function func1() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      const error = true;  //catches error and goes to 'else' if true //catches error and goes to 'if' condition if false
+      if (!error) {
+        console.log("resolved");
+        resolve();
+      } else {
+        console.log("rejected");
+        reject("sorry no fulfilled");
+      }
+    }, 2000);
+  });
+}
+
+func1()
+  .then(function () {
+    console.log("thanks for resolving");
+  })
+  .catch(function () {
+    console.log("not resolved but rejected");
+  });
